2b172d9011d81e5401ba615ec0257c285306f25e
TechnoXtremeDevZ <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2021-10-04 20:16:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+﻿function addUser() {
+    var name1 = document.getElementById("p1_login").value;
+    var name2 = document.getElementById("p2_login").value;
+
+    localStorage.setItem("player1_name", name1);
+    localStorage.setItem("player2_name", name2);
+    window.location = "quiz.html";
+}
---
+++ quiz_login.js
@@ -0,0 +1,15 @@
+﻿var name1 = localStorage.getItem("player1_name");
+var name2 = localStorage.getItem("player2_name");
+var score1 = 0;
+var score2 = 0;
+var player_question = "player1";
+var player_answer = "player2";
+
+document.getElementById("player1_name").innerHTML = name1 + ":";
+document.getElementById("player2_name").innerHTML = name2 + ":";
+
+document.getElementById("player1_score").innerHTML = score1;
+document.getElementById("player1_score").innerHTML = score2;
+
+document.getElementById("player_question").innerHTML = "Question: " + name1;
+document.getElementById("player_answer").innerHTML = "Answer: " + name2;
